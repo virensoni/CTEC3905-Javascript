89f4c7998b2fe1a726c18b609469858314fc144a
Viren Soni <P14176727@LEC-ADMIN.dmu.ac.uk>
2017-10-19 14:38:48 +0100

    made some changes

--- js/greeting.js
+++ js/greeting.js
@@ -3,12 +3,25 @@
   let myName = document.getElementById("my-name"); 
   let userName = document.getElementById("user-name"); 
   let getName = document.getElementById("get-name");
+  let getHeader = document.getElementById("my-header");
 
   function PerformGreeting(ev) {
     myName.innerHTML = userName.value;
+	getHeader.innerHTML = userName.value + "i";
+	
+	
     ev.preventDefault();
+	return false;
   }
 
   getName.addEventListener("submit", PerformGreeting);
+  
+  
+  if (typeof event == "undefined") {
+	  getName.onsubmit = PerformGreeting
+  } else {
+	  getName.addEventListener("submit", PerformGreeting);
+  }
+  
   
 }());
